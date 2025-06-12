e5570a9a4ecf15cb837ce519dbd8e6e4a792931c
samsepiol31 <alexandre.bismuth.iut@gmail.com>
2025-06-12 02:46:55 +0200

    Update login_admin.php

--- site Internet/php/login_admin.php
+++ site Internet/php/login_admin.php
@@ -1,22 +1,17 @@
-<?php
-// login_admin.php - Administrator login page
+<?php 
 session_start();
 
 $error = '';
-// Handle login form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $conn = mysqli_connect('localhost', 'bam', 'PassRoot', 'sae23bam');
-
-    // Sanitize input
+    
     $login = mysqli_real_escape_string($conn, $_POST['login']);
     $mdp = mysqli_real_escape_string($conn, $_POST['motdepasse']);
-
-    // Check admin credentials
+    
     $sql = "SELECT * FROM Administration WHERE Login = '$login' AND MotDePasse = '$mdp'";
     $result = mysqli_query($conn, $sql);
-
+    
     if (mysqli_num_rows($result) === 1) {
-        // Set admin session
         $_SESSION['admin'] = true;
         $_SESSION['last_activity'] = time();
         header('Location: admin.php');
@@ -36,33 +31,31 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="stylesheet" type="text/css" href="../styles/feuille_de_style.css" media="screen"/>
 </head>
 <body>
-<header>
-    <h1>Connexion Administrateur</h1>
-    <nav>
+    <header>
+        <h1>Connexion Administrateur</h1>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Accueil</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <?php if ($error): ?>
+            <div class="error"><?php echo $error; ?></div>
+        <?php endif; ?>
+
+        <form method="post" class="login-form">
+            <input type="text" name="login" placeholder="Login" required>
+            <input type="password" name="motdepasse" placeholder="Mot de passe" required>
+            <input type="submit" value="Connexion">
+        </form>
+    </main>
+
+    <footer>
         <ul>
-            <li><a href="../index.html">Accueil</a></li>
+            <li>ALEXIS BISMUTH MONROUZIES BUT1 R&T</li>
         </ul>
-    </nav>
-</header>
-
-<main>
-    <!-- Display error message if login failed -->
-    <?php if ($error): ?>
-        <div class="error"><?php echo $error; ?></div>
-    <?php endif; ?>
-
-    <!-- Admin login form -->
-    <form method="post" class="login-form">
-        <input type="text" name="login" placeholder="Login" required>
-        <input type="password" name="motdepasse" placeholder="Mot de passe" required>
-        <input type="submit" value="Connexion">
-    </form>
-</main>
-
-<footer>
-    <ul>
-        <li>ALEXIS BISMUTH MONROUZIES BUT1 R&T</li>
-    </ul>
-</footer>
+    </footer>
 </body>
 </html>
